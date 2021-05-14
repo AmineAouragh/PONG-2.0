0d8958ce6395f7c854aa494a8e4e9aa984b67fa8
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-05-14 18:15:48 +0200

    Update start-menu.py

--- start-menu.py
+++ start-menu.py
@@ -14,3 +14,20 @@ def create_surface_with_text(text, font_size, text_rgb, bg_rgb):
     font = pygame.freetype.SysFont("Courier", font_size, bold=True)
     surface, _ = font.render(text=text, fgcolor=text_rgb, bgcolor=bg_rgb)
     return surface.convert_alpha()
+  
+  
+class UIElement(Sprite):
+    """ An user interface element that can be added to a surface """
+    def __init__(self, center_position, text, font_size, bg_rgb, text_rgb):
+
+        self.mouse_over = False  # indicate if the mouse is over the element
+
+        # Create the default image
+        default_image = create_surface_with_text(
+            text=text, font_size=font_size, text_rgb=text_rgb, bg_rgb=bg_rgb
+        )
+
+        # Create the image that shows when mouse is over the element
+        highlighted_image = create_surface_with_text(
+            text=text, font_size=font_size * 1.2, text_rgb=text_rgb, bg_rgb=bg_rgb
+        )
