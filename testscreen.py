f40c7c5bb3d2efe335e253bd6396baa423b3827d
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-06-01 22:43:45 +0200

    Implement a game screen class for refactoring

--- testscreen.py
+++ testscreen.py
@@ -8,25 +8,35 @@ pygame.init()
 
 
 class GameScreen:
-    
+
     sprites_list = []
+    left_paddle = Paddle(colors.WHITE, 20, 120)
+    right_paddle = Paddle(colors.WHITE, 20, 120)
+    ball = Ball(colors.WHITE, 20, 20)
 
     def __init__(self, size, color):
         self.screen = pygame.display.set_mode(size)
         self.color = self.screen.fill(color)
-        
+
     def create_sprites(self, color):
-        
-        self.left_paddle, self.right_paddle = Paddle(color, 20, 120)
+
+        self.left_paddle = Paddle(color, 20, 120)
+        self.right_paddle = Paddle(color, 20, 120)
         self.ball = Ball(color, 20, 20)
-        
-    def draw(self):
-        
-        self.sprites_list = pygame.sprite.Group()    
+
+    def position(self):
+
+        self.left_paddle.rect.x, self.left_paddle.rect.y = 50, 350
+        self.right_paddle.rect.x, self.right_paddle.rect.y = 1120, 350
+        self.ball.rect.x, self.ball.rect.y = 590, 440
+
+    def draw_sprites(self):
+
+        self.sprites_list = pygame.sprite.Group()
         self.sprites_list.add(self.left_paddle, self.right_paddle, self.ball)
         self.sprites_list.draw(self.screen)
-        
-        
+
+
 pygame.display.set_caption("PONG 2.0")
 
 # The clock will be used to control how fast the screen updates
@@ -52,6 +62,9 @@ while True:
                 pygame.quit()
 
     classic = GameScreen((1200, 900), colors.BLACK)
+    classic.create_sprites(colors.WHITE)
+    classic.position()
+    classic.draw_sprites()
 
     # Updating the screen what we've drawn
     pygame.display.flip()
