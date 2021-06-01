563219c067c1474788a51c74348e6bd2f61fa93a
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-06-01 21:51:29 +0200

    Update testscreen.py

--- testscreen.py
+++ testscreen.py
@@ -1,20 +1,32 @@
 # Import the pygame library and initialise the game engine
 import pygame
-from scoredisplayer import ScoreDisplayer
+import colors
 from paddle import Paddle
 from ball import Ball
-import colors
 
 pygame.init()
 
 
 class GameScreen:
+    
+    sprites_list = []
 
     def __init__(self, size, color):
         self.screen = pygame.display.set_mode(size)
         self.color = self.screen.fill(color)
-
-
+        
+    def create_sprites(self, color):
+        
+        self.left_paddle, self.right_paddle = Paddle(color, 20, 120)
+        self.ball = Ball(color, 20, 20)
+        
+    def draw(self):
+        
+        self.sprites_list = pygame.sprite.Group()    
+        self.sprites_list.add(self.left_paddle, self.right_paddle, self.ball)
+        self.sprites_list.draw(self.screen)
+        
+        
 pygame.display.set_caption("PONG 2.0")
 
 # The clock will be used to control how fast the screen updates
